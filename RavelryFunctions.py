b2330a1a0c3c06adf93cf48d4ec3a3dec6feaaaa
yellowcrayon <jcwilcox@engr.ucsb.edu>
2018-03-23 13:05:50 -0700

    Worked on scrapeRavelryPatternData function

--- RavelryFunctions.py
+++ RavelryFunctions.py
@@ -1,6 +1,7 @@
 import time
 import requests as rq
 import math
+import logging
 
 
 def getNestedAttributes(Dict, attrList, levelKey, attrKey):
@@ -223,60 +224,134 @@ def constructPatternTuple(pD):
     return patternTuple
 
 
-def scrapeRavelryPatternData(c, conn, tableName, patternIDs, batchSize, waitTime, user, pswd):
+# Configure the scraper's log file
+logging.basicConfig(filename='scrapeRavelryPatternData.log', level=logging.INFO)
+# ??? Check patternIDs for null values in the main function
+# ??? Check that logging works when I run a scraping test
+
+def scrapeRavelryPatternData(c, conn, tableName, patternIDs, batchSize, waitTime, user, pswd, storedIDsList, failedIDsList):
     """"""  # TODO: Write doc string
+    # batchSize is assumed to be an integer.
+    # batchIDs is assumed to b a non-empty list of integers.
+    # storedIDsList should be [] in the initial function call.
+    # failedIDsList should be [] in the initial function call.
     # Wrap this whole function in a try/except?
 
-    for i in range(0, len(patternIDs), batchSize):  # Split the patternIDs into batches of length batchSize
-        batchIDs = patternIDs[i:i+batchSize]  # Take the desired portion of patternIDs
-        queryString = makePatternQueryString(batchIDs)  # Construct the api query string
-        # print('i = ', i, 'i + batchSize = ', i+batchSize)
-        # print(batchIDs)
-        # print(queryString)
+    if batchSize < 1:  # Recursion stop condition.
 
-        response = rq.get(queryString, auth=(user, pswd))  # Ask Ravelry for the pattern data
+        failedIDsList.append(patternIDs)  # Add this ID to the list of failed IDs.
+        logging.info('batchSize decreased to < 1, skipped ID ' + str(patternIDs) + '.')
+        # If batchSize < 1, then there must be only 1 ID in patternIDs
+        return True  # Return True to go back up the recursion chain.
+
+    for i in range(0, len(patternIDs), batchSize):  # Split the patternIDs into batches of length batchSize.
+        batchIDs = patternIDs[i:i+batchSize]  # Take the desired portion of patternIDs.
+        # If batchSize is greater than the len(batchIDs), then the final batch will be the remaining patternIDs--
+        # all pattern IDs will be covered, and no error will be thrown.
+        queryString = makePatternQueryString(batchIDs)  # Construct the api query string.
+        logging.info('Making API query with batchSize = ' + str(batchSize) + ', using ' + str(len(batchIDs))
+                     + ' IDs from ID ' + str(batchIDs[0]) + ' to ' + str(batchIDs[-1]) + '.')
+
+        response_time_start = time.clock()
+        response = rq.get(queryString, auth=(user, pswd))  # Ask Ravelry for the pattern data.
+        response_time_stop = time.clock()
+
         responseStr = str(response)  # responseStr is the API's response code, e.g. 404, 500, etc.
-        # print(response)
+        logging.info('Received response code ' + responseStr + ' from the API; request took ' +
+                     str(response_time_stop - response_time_start) + ' s to complete.')
 
-        time.sleep(waitTime)  # Wait a moment to avoid spooking the API
+        time.sleep(waitTime)  # Wait a moment to avoid spooking the API.
 
-        if responseStr == '<Response [200]>':  # If the response is successful, parse and save the data to the database
+        if responseStr == '<Response [200]>':  # If the response is successful, parse and save the data to the database.
 
             data = response.json()['patterns']
 
-            for ID in batchIDs:  # Store the data corresponding to each pattern ID one by one
+            storage_time_start = time.clock()
+            for ID in batchIDs:  # Store the data corresponding to each pattern ID one by one.
                 patternData = data[ID]
                 patternDict = parsePatData(patternData)
 
-                if not patternDict:  # If patternDict returns None, then skip this pattern ID
+                if not patternDict:  # If patternDict returns None, then skip this pattern ID.
                     # This means that something serious has gone wrong for this particular pattern ID.
-                    print('Error parsing pattern data, skipping pattern ID ', ID)
-                    break
+                    failedIDsList.append(ID)  # Add this ID to the list of failed IDs
+                    logging.info('Error parsing pattern data, skipping pattern ID ' + str(ID) + '.')
 
-                patternTuple = constructPatternTuple(patternDict)  # Parse our pattern data into an ordered tuple
-                # print(patternTuple)
-                # print(len(response.json()['patterns']))
+                else:
+                    patternTuple = constructPatternTuple(patternDict)  # Parse our pattern data into an ordered tuple.
 
-                # Insert data from this pattern into the table
-                tableString = ''' INSERT OR IGNORE INTO patternData1 VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''  # Ignores entries with a repeated primary key (the pattern ID)
-                c.execute(tableString, patternTuple)
+                    # Insert data from this pattern into the table.
+                    tableString = ''' INSERT OR IGNORE INTO patternData1 VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)'''  # Ignores entries with a repeated primary key (the pattern ID)
+                    c.execute(tableString, patternTuple)
+                    # ??? Write a function to generate the tableString
+
+                    storedIDsList.append(ID)  # Add this ID to the list of IDs whose data were stored in the table.
 
             conn.commit()  # Save the changes to the table
+            storage_time_stop = time.clock()
+            logging.info('It took ' + str(storage_time_stop - storage_time_start) + ' seconds to store data for '
+                         + str(len(batchIDs)) + ' IDs.')
 
-        # If we get a data error,
-        elif responseStr in ['<Response [404]>', '<Response [500]>', '<Response [503]>', '<Response [504]>']:
+        elif responseStr in ['<Response [404]>', '<Response [500]>', '<Response [504]>']:
+            # If we get a data error, reduce the batch size and recursively call this function.
+            # We will query the API with smaller and smaller batches of IDs until we find and skip the IDs that were
+            # causing problems.
+            # We will break the IDs into smaller batches until the batch size gets below 1. This function checks
+            # whether batchSize is < 1 at the top, and returns True if so. Therefore, batch sizes down to batchSize = 1
+            # will be tried before we simply skip that pattern ID.
+            # 404 means that one or more of the patterns in the batch were not found (the corresponding pattern page
+            # may have been deleted, or maybe we somehow input a pattern ID that never existed).
+            # 500 indicates a server error. I have found that sometimes an particular ID will consistently return a 500
+            # response even though my ID scraper found it to be a valid ID--I don't know why this happens, but a
+            # reasonable solution is to just skip these patterns, which is what reducing the batch size will do.
+            # 504 indicates a Gateway Time-out: the request was canceled because it took more than 10 s to generate a
+            # response. Breaking the IDs into smaller batches should fix this problem.
 
-            newBatchSize = 10**math.floor(math.log10(batchSize-1))  # Nearest power of 10 that is smaller than batchSize
-            # e.g. if batchSize = 4291, then newBatchSize = 1000; if batchSize = 100, then newBatchSize = 10
+            if len(batchIDs) < batchSize:
+                # From the way we created batchIDs, batchIDs should always have at least 1 element. However, batchIDs
+                # could end up with far fewer elements than batchSize. For example, let's say we input an initial
+                # patternIDs list with 3 elements, and a batchSize of 100. If one of those IDs gives an error, then
+                # this function will make several API requests with all 3 IDs before batchSize redues to 1.
+                # So if the number of IDs in this batch is less than the current batch size, we'll calculate the new
+                # batch size based on the number of IDs instead of on the previous batchSize.
+                # This condition will also occur on the tail end of a set of batches, e.g. if we have 4021 IDs initially
+                # that get broken into batches of 100 IDs, then the final batch will have only 21 elements.
 
-            print(responseStr, ' in pattern IDs ', batchIDs[0], 'to ', batchIDs[-1], '; reducing batchSize to ', newBatchSize)
-            # Maybe better to store and save a list as a text file so I can go back to the bad sections more easliy?
+                tempBatchSize = len(batchIDs)  # Set a temporary batch size equal to the number of IDs in this batch.
+                newBatchSize = 10 ** math.floor(math.log10(tempBatchSize - 1))  # Nearest power of 10 < number of IDs.
 
-            # Recursively call this function with a smaller batchSize on the current range of batchIDs
-            # ? I think recursively calling this once will work
+            else:
+                newBatchSize = 10**math.floor(math.log10(batchSize-1))  # Nearest power of 10 that is < than batchSize.
+                # e.g. if batchSize = 4291, then newBatchSize = 1000; if batchSize = 100, then newBatchSize = 10.
 
-        # If we get another kind of error,
+            # Recursively call this function with a smaller batchSize on the current range of batchIDs.
+            # I assume that the original batchSize is something fairly small, like 100 or 1000 patterns per request,
+            # which makes recursion appropriate. In my tests so far I have found an initial batch size of 100 or 200
+            # to be best.
+            logging.info(responseStr + ' in pattern IDs ' + str(batchIDs[0]) + ' to '
+                         + str(batchIDs[-1]) + '; reducing batchSize to ' + str(newBatchSize) + '.')
+
+            scrapeRavelryPatternData(c, conn, tableName, batchIDs, newBatchSize, waitTime, user, pswd, storedIDsList, failedIDsList)
+
         else:
-            # ??? What to return? return responseStr
-            print(responseStr,' in pattern IDs ',batchIDs)
+            # If we get another kind of API response code, STOP THE FUNCTION.
+            # Other possible response codes are:
+            # 400 Bad Request: API call is not valid.
+            # 401 Unauthorized: OAuth token has expired or the user has revoked access.
+            # 403 Forbidden: Oauth token is not valid, API keys are not valid, or the user is not permitted to use the
+            # requested method.
+            # 405 Method Not Allowed: you attempted to POST to a GET API method or vice-versa.
+            # 413 Request Entity Too Large: your POST was too large; this function uses a GET API method, so this error
+            # should not come up.
+            # 429 Too Many Requests: If the API says we've sent too many requests, then we should stop sending requests.
+            # I haven't found any documentation that specifies the request limits.
+            # 503 Service Unavailable: returned if the Ravelry API is down / not avaiable. If we get this error, then
+            # we should stop and manually check on the server, and start the function again when the server is back up.
+            msg = responseStr + ' in pattern IDs ' + str(batchIDs[0]) + ' to ' + str(batchIDs[-1]) + '; STOPPING THE FUNCTION.'
+            logging.info(msg)
+            logging.critical(msg)
+
+            raise SystemExit(msg)
+
+    return True  # If the function's largest for loop finishes, this will return True and either go back up the
+    # recursion chain, or end the function.
 
